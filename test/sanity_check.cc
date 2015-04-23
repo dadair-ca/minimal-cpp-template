a8a897bcfd36f6c8b15a29e6b9d9f938beb03652
David Adair <adair.david@gmail.com>
2015-04-23 13:21:31 -0600

    Minimal project template.

---
+++ test/sanity_check.cc
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+TEST(TestSanity, Truth) {
+  ASSERT_TRUE(true);
+}
+
+/* Example test
+TEST(TestSuite, TestName) {
+  ASSERT_TRUE(expected_value, function_call);
+}
+*/
